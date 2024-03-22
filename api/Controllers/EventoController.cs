b335c6dcef71fe3b95a405523d27e3fd4e66f387
LuanGranado <131273881+LuanGranado@users.noreply.github.com>
2024-03-22 16:55:35 -0300

    Criação Evento DAO e Evento Controller

--- api/Controllers/EventoController.cs
+++ api/Controllers/EventoController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using api.DAO;
+using api.Models;
 
 namespace api.Controllers
 {
@@ -10,14 +12,57 @@ namespace api.Controllers
     [Route("api/[controller]")]
     public class EventoController : ControllerBase
     {
-        public class EventoController : ControllerBase
-        {
-            private EventoDAO _eventoDAO;
-        }
+        private EventoDAO _eventoDAO;
 
-        public class EventoController()
+        public EventoController()
         {
             _eventoDAO = new EventoDAO();
+
+        }
+         public IActionResult Get()
+        {
+            var eventos = _eventoDAO.GetAll();
+            return Ok(eventos);
+        }
+
+         [HttpGet("{id}")]
+        public IActionResult GetId(int id)
+        {
+           var eventos = _eventoDAO.GetId(id);
+            if( eventos == null)
+            {
+                return NotFound();
+            }
+            return Ok(eventos);
+        }
+
+         [HttpPost]
+        public IActionResult CriarEvento(Evento evento)
+        {
+            _eventoDAO.CriarEvento(evento);
+            return Ok();
+        }
+
+                [HttpPost("{id}")]
+        public IActionResult AtualizarEvento(int id, Evento evento)
+        {
+            if(_eventoDAO.GetId(id) == null)
+            {
+                return NotFound();
+            }
+            _eventoDAO.AtualizarEvento(id, evento);
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeletarEvento(int id)
+        {
+            if(_eventoDAO.GetId(id) == null)
+            {
+                return NotFound();
+            }
+            _eventoDAO.DeleteEvento(id);
+            return Ok();
         }
     }
 }
--- api/DAO/EventoDAO.cs
+++ api/DAO/EventoDAO.cs
@@ -2,25 +2,201 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using api.Models;
 using api.Repository;
 using MySql.Data.MySqlClient;
 
 
-namespace api.EventoDAO
+namespace api.DAO
 {
     public class EventoDAO
     {
         private MySqlConnection _connection;
 
-
-        public class EventoDAO()
+        public EventoDAO()
         {
             _connection = MySqlConnectionFactory.GetConnection();
         }
 
-        public List<Evento> GetAll()
-        {
-            List<Evento> eventos = new List<Evento>();
+         public List<Evento> GetAll()
+         {
+            List<Evento>  eventos = new List<Evento>();
+            string query = "SELECT * FROM eventos";
+            
+            try
+            {
+                _connection.Open();
+                MySqlCommand command = new MySqlCommand(query, _connection);
+                using (MySqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Evento evento = new Evento
+                        {
+                            IdEvento = reader.GetInt32("id"),
+                            Descricao = reader.GetString("descricao"),
+                            DataEvento = reader.GetDateTime("data_evento"),
+                            TotalIngressos = reader.GetInt32("total_ingressos")
+                        };
+                            eventos.Add(evento);
+                    }
+                }
+            }
+            catch(MySqlException ex)
+            {
+                Console.WriteLine($"Erro do Banco: {ex.Message} ");
+            }
+
+            catch(Exception ex)
+            {
+                Console.WriteLine($"Erro desconhecido{ex.Message}");
+            }
+            
+            finally
+            {
+                _connection.Close();
+            }   
+            return eventos;
         }
-    }
+
+        public Evento GetId(int id)
+        {
+            Evento evento = new Evento();
+            string query = $"SELECT * FROM db_evento.evento Where id = {id}";
+
+             try
+            {
+                _connection.Open();
+                MySqlCommand command = new MySqlCommand(query, _connection);
+                using (MySqlDataReader reader = command.ExecuteReader())
+                {
+                    if(reader.Read())
+                    {
+                            evento.IdEvento = reader.GetInt32("id");
+                            evento.Descricao = reader.GetString("descricao");
+                            evento.DataEvento = reader.GetDateTime("data_evento");
+                            evento.TotalIngressos = reader.GetInt32("total_ingressos");
+                    }
+                }
+               
+            }
+
+            catch(MySqlException ex)
+            {
+                Console.WriteLine($"Erro do Banco: {ex.Message} ");
+            }
+
+            catch(Exception ex)
+            {
+                Console.WriteLine($"Erro desconhecido{ex.Message}");
+            }
+            
+            finally
+            {
+                _connection.Close();
+            }   
+            return evento;
+        }
+
+         public void CriarEvento(Evento evento)
+         {
+            string query = "INSERT INTO evento (id, descricao, data_evento, total_ingressos)" +
+            "values(@Id,  @Descricao, @DataEvento, @TotalIngressos)";
+
+            try
+            {
+                _connection.Open();
+                using (var command = new MySqlCommand(query, _connection))
+                {
+                    command.Parameters.AddWithValue("@Id", evento.IdEvento);
+                    command.Parameters.AddWithValue("@Descricao",evento.Descricao );
+                    command.Parameters.AddWithValue("@DataEvento",evento.DataEvento );
+                    command.Parameters.AddWithValue("@TotalIngressos",evento.TotalIngressos );
+                }
+
+            }
+             catch(MySqlException ex)
+            {
+                Console.WriteLine($"Erro do Banco: {ex.Message} ");
+            }
+
+            catch(Exception ex)
+            {
+                Console.WriteLine($"Erro desconhecido{ex.Message}");
+            }
+            
+            finally
+            {
+                _connection.Close();
+            }   
+         }
+
+           public void AtualizarEvento(int id, Evento evento)
+           {
+            string query = "UPDATE evento SET" +
+                                    "id=@Id," +
+                                    "descricao=@Descricao,"+
+                                    "data_evento=@DataEvento,"+
+                                    "total_ingressos=@TotalIngressos," + 
+                                    "WHERE id=@Id"; 
+            try
+            {
+                _connection.Open();
+                 using (var command = new MySqlCommand(query, _connection))
+                {
+                    command.Parameters.AddWithValue("@Id", evento.IdEvento);
+                    command.Parameters.AddWithValue("@Descricao",evento.Descricao );
+                    command.Parameters.AddWithValue("@DataEvento",evento.DataEvento );
+                    command.Parameters.AddWithValue("@TotalIngressos",evento.TotalIngressos );
+                    command.ExecuteNonQuery();
+                }
+            }
+             catch(MySqlException ex)
+            {
+                Console.WriteLine($"Erro do Banco: {ex.Message} ");
+            }
+
+            catch(Exception ex)
+            {
+                Console.WriteLine($"Erro desconhecido{ex.Message}");
+            }
+            
+            finally
+            {
+                _connection.Close();
+            }   
+
+           }
+
+           public void DeleteEvento(int id)
+           {
+             string query = "DELETE FROM evento WHERE id = @Id";
+
+               try
+            {
+                _connection.Open();
+                using(var command = new MySqlCommand(query, _connection))
+                {
+                    command.Parameters.AddWithValue(@"Id", id);
+                    command.ExecuteNonQuery();
+                }
+            }
+
+            catch(MySqlException ex)
+            {
+                Console.WriteLine($"Erro do Banco: {ex.Message} ");
+            }
+
+            catch(Exception ex)
+            {
+                Console.WriteLine($"Erro desconhecido{ex.Message}");
+            }
+             finally
+            {
+                _connection.Close();
+            }
+           }
+     }
+
+        
 }
